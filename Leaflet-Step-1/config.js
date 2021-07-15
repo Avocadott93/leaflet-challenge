52f49e1d437d28ad6364b33077ad978927ae1e5a
Tina Huang <tinahuang@Tinas-MacBook-Air.local>
2021-07-15 09:49:59 -0400

    upload files

--- Leaflet-Step-1/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoidGluYTkyMzkiLCJhIjoiY2txbDl3bmU1MDRkcTJvbm4ybnp2dGQzYyJ9.iLDQRngeL5i2256zMNYzaw"
---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key from 
+const API_KEY = "pk.eyJ1IjoidGluYTkyMzkiLCJhIjoiY2txc280eHJqM2JzNjJwbWx6YjEwNDRyYiJ9.isqtRzLO6tFpq4YTe_iDPg";
